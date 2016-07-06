45cb7e01cebd23de432f2ead7584ca28e0b4331e
Renguang Dong <dongrenguang@gmail.com>
2016-07-06 16:19:07 +0800

    Modify LESS structure.

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,6 @@ module.exports = {
     context: path.resolve(__dirname, "./src"),
     entry: {
         vendor: ["jquery"],
-        nju: ["./nju/resource/index.less"],
         nm: ["./nm/index.js", "./nm/resource/index.less"]
     },
     output: {
