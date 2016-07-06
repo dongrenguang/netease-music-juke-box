3a35393ea146a1f1f23acc5a544416c22a3aa32f
Renguang Dong <dongrenguang@gmail.com>
2016-07-06 15:58:54 +0800

    Create Application.

--- src/nm/index.js
+++ src/nm/index.js
@@ -1,16 +1,10 @@
-import Panel from "./panel/Panel";
-import PlayListView from "./view/PlayListView";
+import Application from "./app/Application";
 
 function main()
 {
-    const playListView = new PlayListView("play-list");
-
-    const panel = new Panel("panel");
-
-    panel.addSubview(playListView);
-    panel.title = "Title";
-
-    $(document.body).append(panel.$element);
+    const app = new Application();
+    app.placeAt(document.body);
+    app.run();
 }
 
 $(main);
